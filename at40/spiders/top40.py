b0c2258e376d4d14508300ecf0eeeb0aa4e6b0e8
GiladGressel <giladgressel@gmail.com>
2016-03-20 18:36:55 -0700

    updated spider top40.py
    added the correct Url's which we are going to scrape from.  Every single URL we want is now encoded in start_urls

--- at40/spiders/top40.py
+++ at40/spiders/top40.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+def url_creator(year):
+    months = ['01','02','03','04','05','06','07','08','09','10','11','12']
+    stock_url = 'http://www.at40.com/top-40/{}/{}'
+    return [stock_url.format(year,month) for month in months]
 
 class Top40Spider(scrapy.Spider):
     name = "top40"
     allowed_domains = ["at40.com"]
-    start_urls = (
-        'http://www.at40.com/',
-    )
+
+    url = url_creator(2012)
+    url.extend(url_creator(2013))
+    url.extend(url_creator(2014))
+    url.extend(url_creator(2015))
+
+    start_urls = url
 
     def parse(self, response):
         pass
+
+
+
